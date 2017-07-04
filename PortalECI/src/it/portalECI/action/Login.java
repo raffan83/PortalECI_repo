5e499453114907e3afb91d4a543f93c97f216ce2
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2017-07-04 09:34:22 +0200

    commit

--- PortalECI/src/it/portalECI/action/Login.java
+++ PortalECI/src/it/portalECI/action/Login.java
@@ -1,6 +1,8 @@
 package it.portalECI.action;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Login
  */
-@WebServlet("/Login")
+@WebServlet(name="login" , urlPatterns = { "/login.do" })
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -32,7 +34,11 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
+		
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/page/home.jsp");
+     	dispatcher.forward(request,response);
+     	
 	}
 
 }
