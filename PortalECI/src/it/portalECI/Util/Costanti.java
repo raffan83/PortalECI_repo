9b7bc73c69876cb65f767907c2ba46c3a71e048b
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2018-06-18 12:01:37 +0200

    Add Costanti

---
+++ PortalECI/src/it/portalECI/Util/Costanti.java
@@ -0,0 +1,14 @@
+package it.portalECI.Util;
+public class Costanti {
+
+
+
+	public static final String CON_STR_MYSQL = "jdbc:mysql://192.168.3.99:3306/gtv_db?user=root&password=root";
+    
+	public static final String CON_STR_SQLSRV = "jdbc:sqlserver://158.58.172.111:1433;databaseName=BTOMEN_CRESCO_DATI";
+//	public static final String CON_STR_SQLSRV = "jdbc:sqlserver://localhost:1433;databaseName=BTOMEN_CRESCO_DATI";
+	
+    public static final String USR_SQL_SVR = "fantini";
+	public static final String USR_PASS_SVR = "fantini";
+
+}
