4fc0effcd7870f9ca621e5e4861fd3a5c4382638
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2018-06-18 12:17:49 +0200

    Update Costanti

--- PortalECI/src/it/portalECI/Util/Costanti.java
+++ PortalECI/src/it/portalECI/Util/Costanti.java
@@ -3,8 +3,10 @@ public class Costanti {
 
 
 
-	public static final String CON_STR_MYSQL = "jdbc:mysql://192.168.3.99:3306/gtv_db?user=root&password=root";
-    
+	//public static final String CON_STR_MYSQL = "jdbc:mysql://192.168.3.99:3306/gtv_db?user=root&password=root";
+	public static final String CON_STR_MYSQL = "jdbc:mysql://158.58.172.111:3306/gtv_db?user=root&password=root";
+	
+	
 	public static final String CON_STR_SQLSRV = "jdbc:sqlserver://158.58.172.111:1433;databaseName=BTOMEN_CRESCO_DATI";
 //	public static final String CON_STR_SQLSRV = "jdbc:sqlserver://localhost:1433;databaseName=BTOMEN_CRESCO_DATI";
 	
