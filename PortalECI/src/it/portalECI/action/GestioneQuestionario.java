b1f98c9f57f7bb975d02b082f01dc375aa7f3f8c
Rocco Reali <realirocco@gmail.com>
2018-07-11 15:51:52 +0200

    modifica/crea questionario

--- PortalECI/src/it/portalECI/action/GestioneQuestionario.java
+++ PortalECI/src/it/portalECI/action/GestioneQuestionario.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import it.portalECI.DAO.SessionFacotryDAO;
 import it.portalECI.DTO.CategoriaVerificaDTO;
@@ -30,11 +31,19 @@ public class GestioneQuestionario extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		if(Utility.validateSession(request,response,getServletContext()))return;
+		
+		Session session=SessionFacotryDAO.get().openSession();
+
+		ArrayList<TipoVerificaDTO> tipi_verifica = GestioneInterventoBO.getTipoVerifica(session);
+		ArrayList<CategoriaVerificaDTO> categorie_verifica = GestioneInterventoBO.getCategoriaVerifica(session);
+		request.setAttribute("tipi_verifica", tipi_verifica);
+		request.setAttribute("categorie_verifica", categorie_verifica);
 
 		String idQuestionario = request.getParameter("idQuestionario");
 		if(idQuestionario == null || idQuestionario.isEmpty()) {
 			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/page/questionario/formQuestionario.jsp");
 			dispatcher.forward(request,response);
+			session.close();
 			return;
 		}
 
@@ -42,22 +51,96 @@ public class GestioneQuestionario extends HttpServlet {
 		try {
 			id = Integer.parseInt(idQuestionario);
 		}catch (NumberFormatException e) {
-			response.sendRedirect("gestioneListaQuestionari.do");
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			session.close();
 			return;
 		}
 
-		Session session=SessionFacotryDAO.get().openSession();
 
 		QuestionarioDTO questionario = GestioneQuestionarioBO.getQuestionarioById(id, session);
 
-		ArrayList<TipoVerificaDTO> tipi_verifica = GestioneInterventoBO.getTipoVerifica(session);
-		ArrayList<CategoriaVerificaDTO> categorie_verifica = GestioneInterventoBO.getCategoriaVerifica(session);
-
-		request.getSession().setAttribute("questionario", questionario);
-		request.getSession().setAttribute("tipi_verifica", tipi_verifica);
-		request.getSession().setAttribute("categorie_verifica", categorie_verifica);
-
+		request.setAttribute("questionario", questionario);
+		
+		RequestDispatcher dispatcher;
+		if(request.getParameter("action") != null && request.getParameter("action").equals("modifica")) {
+			dispatcher = getServletContext().getRequestDispatcher("/page/questionario/formQuestionario.jsp");
+		}else {
+			dispatcher = getServletContext().getRequestDispatcher("/page/questionario/dettaglioQuestionario.jsp");
+		}
+		dispatcher.forward(request,response);
+		session.close();
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if(Utility.validateSession(request,response,getServletContext()))return;
+		
+		if(request.getParameter("_method")!= null && request.getParameter("_method").equalsIgnoreCase("PUT")) {
+			doPut(request,response);
+			return;
+		}
+		
+		Session session=SessionFacotryDAO.get().openSession();
+		
+		QuestionarioDTO questionario = new QuestionarioDTO();
+		
+		questionario = setQuestionarioFromRequest(request, questionario, session);
+		
+		Transaction transaction = session.beginTransaction();
+		session.save(questionario);
+		transaction.commit();
+		
+		request.setAttribute("questionario", questionario);
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/page/questionario/dettaglioQuestionario.jsp");
 		dispatcher.forward(request,response);
+		session.close();
+	
+	}
+	
+	public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if(Utility.validateSession(request,response,getServletContext()))return;
+		
+		Session session=SessionFacotryDAO.get().openSession();
+		
+		String idQuestionario = request.getParameter("idQuestionario");
+		
+		Integer id = null;
+		try {
+			id = Integer.parseInt(idQuestionario);
+		}catch (NumberFormatException e) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			session.close();
+			return;
+		}
+		
+		QuestionarioDTO questionario = GestioneQuestionarioBO.getQuestionarioById(id, session);
+		
+		if(questionario == null) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			session.close();
+			return;
+		}
+		
+		questionario = setQuestionarioFromRequest(request, questionario, session);
+		
+		Transaction transaction = session.beginTransaction();
+		session.update(questionario);
+		transaction.commit();
+		request.setAttribute("questionario", questionario);
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/page/questionario/dettaglioQuestionario.jsp");
+		dispatcher.forward(request, response);
+		session.close();
+
+	}
+	
+	private QuestionarioDTO setQuestionarioFromRequest(HttpServletRequest request, QuestionarioDTO questionario, Session hibernateSession) {
+		questionario.setTitolo(request.getParameter("titolo"));
+		
+		String tipoQuestionarioValue = request.getParameter("tipo");
+		String[] tipoQuestionario = tipoQuestionarioValue.split("_");
+		questionario.setTipo(GestioneInterventoBO.getTipoVerifica(tipoQuestionario[0], hibernateSession));
+		
+		return questionario;
 	}
 }
