44b7b38953dc9fea456d56fc903d449153af5d83
Rocco Reali <realirocco@gmail.com>
2018-07-19 16:45:28 +0200

    Suggerimenti per placeholders su form del template questionario

--- PortalECI/WebContent/js/template-questionario.js
+++ PortalECI/WebContent/js/template-questionario.js
@@ -1,41 +1,38 @@
+var PLACEHOLDERS = [];
+
 $(document).ready(function() {
 	$('#summernote').summernote({
 		height : 200,
 		tabsize : 2,
 		lang : 'it-IT',
-		hint : {
-			tags : [ 'apple', 'orange', 'watermelon', 'lemon' ],
-			match : /\{\w*$/,
-			search : function(keyword, callback) {
-				callback($.grep(Object.keys(this.tags), function(item) {
-					if (!keyword)
-						return true
-					else
-						return item.indexOf(keyword) == 0;
+		hint: {
+			match: /\B\$\{(\w*)$/,
+			search: function (keyword, callback) {
+				callback($.grep(PLACEHOLDERS, function (item) {
+					return item.toUpperCase().indexOf(keyword.toUpperCase())  == 0;
 				}));
 			},
-			template : function(item) {
-				return '{' + item + '} : ' + this.tags[item];
+			template: function (item) {
+				return '${'+item+'}';
 			},
-			content : function(item) {
-				return '{' + item + '}';
+			content: function (item) {
+				return '${'+item+'}';
 			}
-		}
-	});
-})
 
-function test() {
+		  }
+	});
+	
 	$.ajax({
 		type : "GET",
 		url : "gestionePlaceholder.do",
 		data : "",
 		dataType : "json",
 		success : function(data, textStatus) {
-			console.log(data);
+			PLACEHOLDERS=data;
 		},
-
 		error : function(jqXHR, textStatus, errorThrown) {
 			console.log(textStatus);
 		}
 	});
-}
+
+})
