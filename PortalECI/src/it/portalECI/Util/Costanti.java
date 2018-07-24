9979f0d4cac4c425f3fbbfee43ea832732b196a3
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2018-07-24 11:27:04 +0200

    comm

--- PortalECI/src/it/portalECI/Util/Costanti.java
+++ PortalECI/src/it/portalECI/Util/Costanti.java
@@ -3,15 +3,15 @@ public class Costanti {
 
 
 
-	//public static final String CON_STR_MYSQL = "jdbc:mysql://192.168.3.99:3306/gtv_db?user=root&password=root";
+	public static final String CON_STR_MYSQL = "jdbc:mysql://158.58.172.111:3306/gtv_db?user=root&password=root";
 	//public static final String CON_STR_MYSQL = "jdbc:mysql://158.58.172.111:3306/gtv_db?user=root&password=root";
 	//IP MACROSOLUTION
-	public static final String CON_STR_MYSQL = "jdbc:mysql://212.237.23.231:3306/gtv_db?user=root&password=root";
+//	public static final String CON_STR_MYSQL = "jdbc:mysql://212.237.23.231:3306/gtv_db?user=root&password=root";
 	
 	
-	//public static final String CON_STR_SQLSRV = "jdbc:sqlserver://158.58.172.111:1433;databaseName=BTOMEN_CRESCO_DATI";
+	public static final String CON_STR_SQLSRV = "jdbc:sqlserver://158.58.172.111:1433;databaseName=BTOMEN_CRESCO_DATI";
 	//IP MACROSOLUTION
-	public static final String CON_STR_SQLSRV = "jdbc:sqlserver://212.237.23.231:1433;databaseName=BTOMEN_CRESCO_DATI";
+//	public static final String CON_STR_SQLSRV = "jdbc:sqlserver://212.237.23.231:1433;databaseName=BTOMEN_CRESCO_DATI";
 //	public static final String CON_STR_SQLSRV = "jdbc:sqlserver://localhost:1433;databaseName=BTOMEN_CRESCO_DATI";
 	
     public static final String USR_SQL_SVR = "fantini";
