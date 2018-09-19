c36c73fddc99240908ebcd23e5357ed90d462e60
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2018-09-19 15:01:12 +0200

    Update pwd Mysql

--- PortalECI/src/it/portalECI/Util/Costanti.java
+++ PortalECI/src/it/portalECI/Util/Costanti.java
@@ -7,7 +7,7 @@ public class Costanti {
 
 
 
-	public static final String CON_STR_MYSQL = "jdbc:mysql://158.58.172.111:3306/gtv_db?user=root&password=root";
+	public static final String CON_STR_MYSQL = "jdbc:mysql://158.58.172.111:3306/gtv_db?user=root&password=5243FWF5344R";
 	//public static final String CON_STR_MYSQL = "jdbc:mysql://158.58.172.111:3306/gtv_db?user=root&password=root";
 	//IP MACROSOLUTION
 //	public static final String CON_STR_MYSQL = "jdbc:mysql://212.237.23.231:3306/gtv_db?user=root&password=root";
