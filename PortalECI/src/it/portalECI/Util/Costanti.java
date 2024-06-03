e32aef14201d5f3a1e92d05c4b34e2766b4555b9
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2024-06-03 09:22:05 +0200

    aggiornamento DB

--- PortalECI/src/it/portalECI/Util/Costanti.java
+++ PortalECI/src/it/portalECI/Util/Costanti.java
@@ -8,7 +8,7 @@ public class Costanti {
 
 
 //public static final String CON_STR_MYSQL = "jdbc:mysql://158.58.172.111:3306/gtv_db?user=root&password=5243FWF5344R";
-	public static final String CON_STR_MYSQL = "jdbc:mysql://158.58.172.111:3306/eci_db?user=root&password=5243FWF5344R";
+	public static final String CON_STR_MYSQL = "jdbc:mysql://89.46.227.105:3306/eci_db?user=root&password=5243FWF5344R";
 //  IP MACROSOLUTION
 //	public static final String CON_STR_MYSQL = "jdbc:mysql://212.237.23.231:3306/gtv_db?user=root&password=5243FWF5344R";
 	
